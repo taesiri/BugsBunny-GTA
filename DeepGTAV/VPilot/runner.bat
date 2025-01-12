7c6adb300365cb22cd32f506f610cfac88430833
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2025-01-11 19:25:49 -0700

    Update runner.bat

--- DeepGTAV/VPilot/runner.bat
+++ DeepGTAV/VPilot/runner.bat
@@ -1,9 +1,12 @@
 @echo off
 setlocal
 
+set BASE_SAVE_DIR=C:\Workspace\gta_data_export\capture_
+
 for /l %%i in (1,1,10) do (
     echo Running iteration %%i...
-    python BugsBunny-Getdata-FlyingCar.py --random_location --camera_preset third_person --frames_to_capture 100
+    set "SAVE_DIR=%BASE_SAVE_DIR%%%i"
+    python BugsBunny-Getdata-FlyingCar.py --random_location --camera_preset first_person --frames_to_capture 100 --save_dir "%SAVE_DIR%"
     echo Waiting for 10 seconds before the next run...
     timeout /t 10 /nobreak >nul
 )
